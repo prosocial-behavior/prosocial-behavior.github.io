1fb9be4e7792d73f5551482fabeb5b0a3419f5eb
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2025-09-14 01:34:15 +0300

    changes

--- data.js
+++ data.js
@@ -1,14 +1,56 @@
 const data = [
     {
-        image: '1.png',
-        answears: ['Оценяване на красивото', 'Неоценяване на красивото'],
-        correctAnwear: 'Оценяване на красивото'
+        type: 'init'
     },
     {
-        image: '2.png',
-        answears: ['Оценяване на красивото', 'Неоценяване на красивото'],
-        correctAnwear: 'Неоценяване на красивото'
+        type: 'init_1'
+    },
+    {
+        type: 'info',
+        image: 'images/cards/1.jpg',
+        title: 'Оценяване на красивото',
+        text: 'Да забележиш и оцениш на красотата, превъзходството и/или умелото изпълнение в различни области на живота, от природата до изкуството, математиката, науката и ежедневния опит. Да се ​​наслаждаваш на това, което виждаш в себе си, в другите и в света.',
+        examples: [
+            'Да обичаш да си сред природата',
+            'Да оценяваш, когато нещата са добре направени',
+            'Да намираш и откриваш красотата във всички области на живота, от природата до изкуствата, математиката и науката'
+        ]
+    },
+    {
+        type: 'question',
+        image: 'images/questions/1.jpg',
+        answerOne: 'Оценяване на красивото',
+        answerTwo: 'Неоценяване на красивото',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/2.jpg',
+        answerOne: 'Оценяване на красивото',
+        answerTwo: 'Неоценяване на красивото',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/3.jpg',
+        answerOne: 'Оценяване на красивото',
+        answerTwo: 'Неоценяване на красивото',
+        correctAnswer: 'two'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/4.jpg',
+        answerOne: 'Оценяване на красивото',
+        answerTwo: 'Неоценяване на красивото',
+        correctAnswer: 'two'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/5.jpg',
+        answerOne: 'Оценяване на красивото',
+        answerTwo: 'Неоценяване на красивото',
+        correctAnswer: 'one'
     }
 ];
 
-exports.module = data;
+export default data;
