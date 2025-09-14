79c746ec49da93b3a7fccbf181705ae8452c526c
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2025-09-14 19:50:13 +0300

    changes

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const data = [
     {
         type: 'info',
         image: 'images/cards/1.jpg',
-        title: 'Оценяване на красивото',
+        title: 'Оценяване на красотата и съвършенството',
         text: 'Да забележиш и оцениш красотата, превъзходството и/или умелото изпълнение в различни области на живота, от природата до изкуството, математиката, науката и ежедневния опит. Да се ​​наслаждаваш на това, което виждаш в себе си, в другите и в света.',
         examples: [
             'Да обичаш да си сред природата',
@@ -19,36 +19,36 @@ const data = [
     {
         type: 'question',
         image: 'images/questions/1.jpg',
-        answerOne: 'Оценяване на красивото',
-        answerTwo: 'Неоценяване на красивото',
+        answerOne: 'Оценяване на красотата',
+        answerTwo: 'Неоценяване на красотата',
         correctAnswer: 'one'
     },
     {
         type: 'question',
         image: 'images/questions/2.jpg',
-        answerOne: 'Оценяване на красивото',
-        answerTwo: 'Неоценяване на красивото',
+        answerOne: 'Оценяване на красотата',
+        answerTwo: 'Неоценяване на красотата',
         correctAnswer: 'one'
     },
     {
         type: 'question',
         image: 'images/questions/3.jpg',
-        answerOne: 'Оценяване на красивото',
-        answerTwo: 'Неоценяване на красивото',
+        answerOne: 'Оценяване на красотата',
+        answerTwo: 'Неоценяване на красотата',
         correctAnswer: 'two'
     },
     {
         type: 'question',
         image: 'images/questions/4.jpg',
-        answerOne: 'Оценяване на красивото',
-        answerTwo: 'Неоценяване на красивото',
+        answerOne: 'Оценяване на красотата',
+        answerTwo: 'Неоценяване на красотата',
         correctAnswer: 'two'
     },
     {
         type: 'question',
         image: 'images/questions/5.jpg',
-        answerOne: 'Оценяване на красивото',
-        answerTwo: 'Неоценяване на красивото',
+        answerOne: 'Оценяване на красотата',
+        answerTwo: 'Неоценяване на красотата',
         correctAnswer: 'one'
     },
     {
@@ -62,7 +62,89 @@ const data = [
             'Да приемаш предизвикателства',
             'Да предприемаш действия, с които да помагаш на другите'
         ]
-    }
+    },
+    {
+        type: 'question',
+        image: 'images/questions/6.jpg',
+        answerOne: 'Смелост',
+        answerTwo: 'Липса на смелост',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/7.jpg',
+        answerOne: 'Смелост',
+        answerTwo: 'Липса на смелост',
+        correctAnswer: 'two'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/8.jpg',
+        answerOne: 'Смелост',
+        answerTwo: 'Липса на смелост',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/9.jpg',
+        answerOne: 'Смелост',
+        answerTwo: 'Липса на смелост',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/10.jpg',
+        answerOne: 'Смелост',
+        answerTwo: 'Липса на смелост',
+        correctAnswer: 'two'
+    },
+    {
+        type: 'info',
+        image: 'images/cards/3.jpg',
+        title: 'Креативност',
+        text: 'Мислене за нови и продуктивни начини за концептуализиране и правене на неща; включва художествени постижения, но не се ограничава до тях. Мислене за нови начини за правене на неща.',
+        examples: [
+            'Да обичаш да използваш въображението си, за да решаваш различни проблеми',
+            'Да опитваш нови и вълнуващи начини за правене на нещата',
+            'Да бъдеш артистичен',
+            'Да приговряш нова и по-сложна "рецепта"'
+        ]
+    },
+    {
+        type: 'question',
+        image: 'images/questions/11.jpg',
+        answerOne: 'Креативност',
+        answerTwo: 'Липса на креативност',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/12.jpg',
+        answerOne: 'Креативност',
+        answerTwo: 'Липса на креативност',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/13.jpg',
+        answerOne: 'Креативност',
+        answerTwo: 'Липса на креативност',
+        correctAnswer: 'two'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/14.jpg',
+        answerOne: 'Креативност',
+        answerTwo: 'Липса на креативност',
+        correctAnswer: 'one'
+    },
+    {
+        type: 'question',
+        image: 'images/questions/15.jpg',
+        answerOne: 'Креативност',
+        answerTwo: 'Липса на креативност',
+        correctAnswer: 'two'
+    },
 ];
 
 export default data;
